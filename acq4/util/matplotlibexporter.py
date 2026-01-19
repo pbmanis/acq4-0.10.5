0d4b26bb773d22eda357807eb7abb7005038f6ca
Paul B. Manis <pmanis@med.unc.edu>
2026-01-19 11:36:38 -0500

    Remove tkagg dependency; set usetex false

--- acq4/util/matplotlibexporter.py
+++ acq4/util/matplotlibexporter.py
@@ -20,19 +20,18 @@ except ImportError:
     HAVE_MPL = False
 
 if HAVE_MPL:
-    MP.use('TKAgg')
+
     # Do not modify the following code
     # sets up matplotlib with sans-serif plotting...
-    PL.rcParams['text.usetex'] = True
+    PL.rcParams['text.usetex'] = False
     PL.rcParams['interactive'] = False
     PL.rcParams['font.family'] = 'sans-serif'
     PL.rcParams['font.sans-serif'] = 'Arial'
     PL.rcParams['mathtext.default'] = 'sf'
     PL.rcParams['figure.facecolor'] = 'white'
     # next setting allows pdf font to be readable in Adobe Illustrator
-    PL.rcParams['pdf.fonttype'] = 42
-    PL.rcParams['text.dvipnghack'] = True
-    # to here (matplotlib stuff - touchy!)
+    PL.rcParams['pdf.fonttype'] = 42 
+        # to here (matplotlib stuff - touchy!)
 
 stdFont = 'Arial'
 
@@ -73,7 +72,7 @@ def matplotlibExport(gridlayout=None, title=None):
         raise Exception("Method matplotlibExport requires a QGridLayout")
 
     fig = PL.figure()
-    PL.rcParams['text.usetex'] = False
+
     # escape filename information so it can be rendered by removing
     # common characters that trip up latex...:
     escs = re.compile('[\\\/_]')
