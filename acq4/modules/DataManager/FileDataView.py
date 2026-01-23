c2167e75718d578354810b2a2b47848ca24b0c1e
Paul B. Manis <pmanis@med.unc.edu>
2026-01-23 09:12:26 -0500

    add back multiplotwidget...

--- acq4/modules/DataManager/FileDataView.py
+++ acq4/modules/DataManager/FileDataView.py
@@ -6,141 +6,9 @@ from acq4.util import Qt
 from acq4.util.DataManager import FileHandle
 from acq4.util.DictView import DictView
 
-# from pg.graphicsItems import MultiPlotItem as MultiPlotItem
 from pyqtgraph.Qt import QtCore
-from pyqtgraph.widgets import GraphicsView
-from pyqtgraph.graphicsItems import GraphicsLayout
-
-
-
-class MultiPlotItem(GraphicsLayout.GraphicsLayout):
-    """
-    :class:`~pyqtgraph.GraphicsLayout` that automatically generates a grid of
-    plots from a MetaArray.
-
-    .. seealso:: :class:`~pyqtgraph.MultiPlotWidget`: Widget containing a MultiPlotItem
-    """
-
-    def __init__(self, *args, **kwds):
-        GraphicsLayout.GraphicsLayout.__init__(self, *args, **kwds)
-        self.plots = []
-
-    def plot(self, data, **plotArgs):
-        """Plot the data from a MetaArray with each array column as a separate
-        :class:`~pyqtgraph.PlotItem`.
-
-        Axis labels are automatically extracted from the array info.
-
-        ``plotArgs`` are passed to :meth:`PlotItem.plot
-        <pyqtgraph.PlotItem.plot>`.
-        """
-        #self.layout.clear()
-
-        if hasattr(data, 'implements') and data.implements('MetaArray'):
-            if data.ndim != 2:
-                raise Exception("MultiPlot currently only accepts 2D MetaArray.")
-            ic = data.infoCopy()
-            ax = 0
-            for i in [0, 1]:
-                if 'cols' in ic[i]:
-                    ax = i
-                    break
-            #print "Plotting using axis %d as columns (%d plots)" % (ax, data.shape[ax])
-            for i in range(data.shape[ax]):
-                pi = self.addPlot()
-                self.nextRow()
-                sl = [slice(None)] * 2
-                sl[ax] = i
-                pi.plot(data[tuple(sl)], **plotArgs)
-                #self.layout.addItem(pi, i, 0)
-                self.plots.append((pi, i, 0))
-                info = ic[ax]['cols'][i]
-                title = info.get('title', info.get('name', None))
-                units = info.get('units', None)
-                pi.setLabel('left', text=title, units=units)
-            info = ic[1-ax]
-            title = info.get('title', info.get('name', None))
-            units = info.get('units', None)
-            pi.setLabel('bottom', text=title, units=units)
-        else:
-            raise Exception("Data type %s not (yet?) supported for MultiPlot." % type(data))
-
-    def close(self):
-        for p in self.plots:
-            p[0].close()
-        self.plots = None
-        self.clear()
-
-
-class MultiPlotWidget(GraphicsView):
-    """Widget implementing a :class:`~pyqtgraph.GraphicsView` with a single
-    :class:`~pyqtgraph.MultiPlotItem` inside."""
-    def __init__(self, parent=None):
-        self.minPlotHeight = 50
-        self.mPlotItem = MultiPlotItem()
-        GraphicsView.__init__(self, parent)
-        self.enableMouse(False)
-        self.setCentralItem(self.mPlotItem)
-        ## Explicitly wrap methods from mPlotItem
-        #for m in ['setData']:
-            #setattr(self, m, getattr(self.mPlotItem, m))
-        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAsNeeded)
-        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAsNeeded)
-                
-    def __getattr__(self, attr):  ## implicitly wrap methods from plotItem
-        if hasattr(self.mPlotItem, attr):
-            m = getattr(self.mPlotItem, attr)
-            if hasattr(m, '__call__'):
-                return m
-        raise AttributeError(attr)
-
-    def setMinimumPlotHeight(self, min):
-        """Set the minimum height for each sub-plot displayed. 
-        
-        If the total height of all plots is greater than the height of the 
-        widget, then a scroll bar will appear to provide access to the entire
-        set of plots.
-        
-        Added in version 0.9.9
-        """
-        self.minPlotHeight = min
-        self.resizeEvent(None)
-
-    def widgetGroupInterface(self):
-        return (None, MultiPlotWidget.saveState, MultiPlotWidget.restoreState)
-
-    def saveState(self):
-        return {}
-        #return self.plotItem.saveState()
-        
-    def restoreState(self, state):
-        pass
-        #return self.plotItem.restoreState(state)
-
-    def close(self):
-        self.mPlotItem.close()
-        self.mPlotItem = None
-        self.setParent(None)
-        GraphicsView.close(self)
-
-    def setRange(self, *args, **kwds):
-        GraphicsView.setRange(self, *args, **kwds)
-        if self.centralWidget is not None:
-            r = self.range
-            minHeight = len(self.mPlotItem.plots) * self.minPlotHeight
-            if r.height() < minHeight:
-                r.setHeight(minHeight)
-                r.setWidth(r.width() - self.verticalScrollBar().width())
-            self.centralWidget.setGeometry(r)
-
-    def resizeEvent(self, ev):
-        if self.closed:
-            return
-        if self.autoPixelRange:
-            self.range = QtCore.QRectF(0, 0, self.size().width(), self.size().height())
-        MultiPlotWidget.setRange(self, self.range, padding=0, disableAutoPixel=False)  ## we do this because some subclasses like to redefine setRange in an incompatible way.
-        self.updateMatrix()
-
+from pyqtgraph.widgets import GraphicsView, MultiPlotWidget
+from pyqtgraph.graphicsItems import GraphicsLayout, MultiPlotItems
 
 
 class FileDataView(Qt.QSplitter):
@@ -196,6 +64,8 @@ class FileDataView(Qt.QSplitter):
 
     def displayDataAsPlot(self, data):
         self.clear()
+        GraphicsView.__init__(self)
+
         w = MultiPlotWidget(self)
         w.setObjectName("DataManager_multiPlotWidget")
         self.addWidget(w)
